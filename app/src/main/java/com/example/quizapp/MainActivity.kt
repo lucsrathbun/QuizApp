068fd69627d8a59b3332e4fbb5cdbf2a6345df3e
lucsrathbun <lucsrathbun@gmail.com>
2022-10-05 10:10:31 -0700

    added questions

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = "MainActivity"
+    }
+    private lateinit var quiz: Quiz
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        importQuestions()
+
+        //get the first questions, set up the textviews, set up the onclicklisteners for the buttons
     }
+
+    private fun importQuestions() {
+        // step 1: open the raw resource as an InputStream
+        // R.raw.questions --> R is the Resources class, raw is folder,
+        // questions is the file
+        val inputStream = resources.openRawResource(R.raw.questions)
+        // step 2: use a buffered reader on the inputstream to read the
+        // the text into a string. we call it jsonString because it's the entire JSON file in a string
+        val jsonString = inputStream.bufferedReader().use {
+            // the last line of the use function is returned
+            it.readText()
+        }
+
+        Log.d(TAG, "onCreate: $jsonString")
+
+        val gson = Gson()
+        // gson needs to know what kind of list you are converting to
+        // typetoken tells gson it will be a List<Question>
+
+        val type = object : TypeToken<List<Questions>>() { }.type
+        val questions = gson.fromJson<List<Questions>>(jsonString, type)
+
+        Log.d(TAG, "onCreate: $questions")
+
+        //create the quiz object from the list of questions
+        quiz = Quiz(questions)
+    }
+
 }
---
+++ app/src/main/java/com/example/quizapp/Quiz.kt
@@ -0,0 +1,6 @@
+package com.example.quizapp
+
+class Quiz(val questions: List<Questions>) {
+    // varaibles to track score, current questions
+    //functions to check if there's another question, give the final score, give the next question, check the answer, reset the quiz?, shuffle?
+}
